44424051730aac2e703d21c14778798f21cb26fe
Himani Jangid <himanijangid07@gmail.com>
2024-08-08 17:33:03 +0530

    page updated

--- script.js
+++ script.js
@@ -46,3 +46,15 @@ function openInsurance() {
   open.style.display = "none";
   close1.style.display = "flex";
 }
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+  } else {
+      $('.back-to-top').fadeOut('slow');
+  }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
